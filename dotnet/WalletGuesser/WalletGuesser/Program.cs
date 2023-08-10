98b80707a9ee1fbe7261bca95226befee8bdca6b
Troy <troy.lamerton@gmail.com>
2023-08-11 08:11:06 +1200

    throw exception when complete

--- dotnet/WalletGuesser/WalletGuesser/Program.cs
+++ dotnet/WalletGuesser/WalletGuesser/Program.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Collections;
 using System.Linq;
 using Nethereum.HdWallet;
+using System.Reflection;
 
 // read list of english words
 // Converted from UnityScript to C# at http://www.M2H.nl/files/js_to_c.php - by Mike Hergaarden
@@ -12,49 +13,68 @@ public class Program {
     public static void Main(string[] args) {
         // array of possible words used in the pass phrase
         var words = File.ReadAllLines("english.txt").ToArray();
-        Parallel.For(0, 2048, index =>
+        CancellationTokenSource cts = new CancellationTokenSource();
+
+        // Use ParallelOptions instance to store the CancellationToken
+        ParallelOptions po = new ParallelOptions();
+        po.CancellationToken = cts.Token;
+        po.MaxDegreeOfParallelism = Environment.ProcessorCount;
+
+        var countDone = 0;
+        var countDone2 = 0;
+        var sw = Stopwatch.StartNew();
+        foreach (var word1 in words)
         {
-            var word1 = words[index];
-            if (index % 100 == 0) {
-                Console.WriteLine($"{index}/${words.Length}");
-            }
-            foreach (var word2 in words) {
-                if (word2 == word1) continue;
-                foreach (var word3 in words) {
-                    if (word3 == word1) continue;
-                    if (word3 == word2) continue;
+            if (cts.IsCancellationRequested) break;
+            //if (countDone % 2 == 0) {
+            //    Console.WriteLine($"{countDone}/{words.Length}");
+            //}
+            countDone++;
+            countDone2 = 0;
+            //foreach (var word2 in words) {
+                //if (cts.IsCancellationRequested) break;
+            Parallel.For(0, 2048, po, index2 => {
+                var word2 = words[index2];
+                countDone2++;
+                if (word2 == word1) return;
+
+                if (countDone2 % 2 == 0) {
+                    Console.WriteLine($"{countDone}/2048  {countDone2}/2048  {sw.Elapsed.TotalSeconds:F1}");
+                }
+                Parallel.For(0, 2048, po, index => {
+                    var word3 = words[index];
+                    if (word3 == word1) return;
+                    if (word3 == word2) return;
                     try {
-                        if ((word1 == "elbow" || word2 == "elbow" || word3 == "elbow")
-                        // && (word1 == "tennis" || word2 == "tennis" || word3 == "tennis")
-                        ) {
-                            testAddress($"{word1} {word2} {word3}");
-                        }
+                        testAddress($"{word1} {word2} {word3}");
                     } catch (Exception error) {
-                        if (error.HResult == 111) { // TODO: invalid mnemonic hash
+                        if (error.HResult == 999999090) { // TODO: invalid mnemonic hash
                                                     // random words dont always make a valid mnemonic
                         }
                         else {
                             Console.WriteLine($"{word1} {word2} {word3}");
                             Console.WriteLine(error.ToString());
+                            cts.Cancel();
                             return;
                         }
                     }
-                }
-            }
-        });
+                });
+            });
+        }
     }
 
     static void testAddress(string words) {
-        var phrase = Environment.GetEnvironmentVariable("KNOWN_PHRASE_START").Trim() + ' ' + words;
+        var phrase = Environment.GetEnvironmentVariable("KNOWN_PHRASE_START").Trim() + " " + words;
         //Nethereum.Web3.Accounts.Account
-        var wallet3 = new Wallet(words, "password2");
-        var wallet = wallet3.GetAccount(0);
-
+        var wallet = new Wallet(phrase, "password2");
+        var addresses = wallet.GetAddresses(1);
+        var address = addresses[0];
         //var wallet = ethers.Wallet.fromPhrase(phrase);
         // expected address: 0x98C6f7592653B3c74D43bedCd8199AEfdc59E2fA
-        if (wallet.Address.StartsWith("0x98C6")) {
-            Console.WriteLine(wallet.Address);
-            Console.WriteLine(words);
+        if (address.StartsWith("0x98C6f7592653B3c")) {
+            Console.WriteLine(address);
+            Console.WriteLine(phrase);
+            throw new Exception("DONE");
         }
     }
 
